f76fb0101d5d291c433eec1af17e919eaf4f20fe
Chris Coyier <chriscoyier@gmail.com>
2014-08-23 20:37:19 -0700

    no hero except home

--- js/site.js
+++ js/site.js
@@ -12,14 +12,11 @@
     },
 
     kickOffAnimations: function() {
-      this.drawPath("shape-prism-1");
+      
     },
 
     bindEventAnimations: function() {
-      // var prism1 = document.getElementById("shape-prism-1");
-      // prism1.addEventListener("mouseenter", function() {
-      //   Site.drawPath("shape-prism-1");
-      // });
+      
     },
 
     drawPath: function(id) {
@@ -28,22 +25,17 @@
 
       var length = path.getTotalLength();
 
-      // Clear any previous transition
       path.style.transition = path.style.WebkitTransition =
         "none";
 
-      // Set up the starting positions
       path.style.strokeDasharray = length + " " + length;
       path.style.strokeDashoffset = length;
 
-      // Trigger a layout so styles are calculated & the browser
-      // picks up the starting position before animating
       path.getBoundingClientRect();
 
-      // Define our transition
       path.style.transition = path.style.WebkitTransition =
         "stroke-dashoffset 1s ease-in-out";
-      // Go!
+      
       path.style.strokeDashoffset = "0";
 
     }
